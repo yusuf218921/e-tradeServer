108ba793d197501c42f0e068c0fec946e155d023
Yusuf <yusufsonmez951@gmail.com>
2023-07-28 23:39:17 +0300

    katmanlı mimari oluşturuldu

---
+++ src/main/java/com/ovammarket/etrade/EtradeApplication.java
@@ -0,0 +1,13 @@
+package com.ovammarket.etrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ovammarket/etrade/EtradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovammarket.etrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
